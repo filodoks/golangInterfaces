a1f48cdac4b22006de01f528cc8867169ae36c99
filodoks <filodoks@gmail.com>
2019-12-19 11:21:54 +0100

    06 - 067

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,9 +16,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	// io.Copy(os.Stdout, resp.Body)
+
 }
 
-// 06 - 062
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
+
+// 06 - 67
